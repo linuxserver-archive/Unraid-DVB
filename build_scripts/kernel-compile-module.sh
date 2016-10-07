a49a5ed8b2bfd66bba93acf42dcb4bf15f774c09
Neil <dr.neil.a.green@gmail.com>
2016-10-07 17:31:57 +0100

    Update kernel-compile-module.sh

--- build_scripts/kernel-compile-module.sh
+++ build_scripts/kernel-compile-module.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+##Clean up working directory
+rm -rf $D/bzroot-dd $D/bzroot-libreelec $D/bzroot-tbs-* $D/kernel $D/lib $D/libreelec-drivers $D/packages $D/tbs-drivers-* $D/unraid $D/FILE_LIST $D/linux-*.tar.xz $D/unRAIDServer-*.zip $D/URLS $D/variables.sh
+
 ##Pull variables from github
 wget -nc https://raw.githubusercontent.com/CHBMB/Unraid-DVB/master/files/variables.sh
 . "$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"/variables.sh
