78eb900a944aa02eaee3057791e4eaf1b335d135
Neil <dr.neil.a.green@gmail.com>
2016-11-16 17:15:13 +0000

    Update tbs-os.sh

--- build_scripts/tbs-os.sh
+++ build_scripts/tbs-os.sh
@@ -2,10 +2,6 @@
 
 ###Run kernel_compile.sh prior to running a module###
 
-#!/bin/bash
-
-###Run kernel_compile.sh prior to running a module###
-
 ##Pull variables from github
 wget -nc https://raw.githubusercontent.com/CHBMB/Unraid-DVB/master/files/variables.sh
 . "$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"/variables.sh
