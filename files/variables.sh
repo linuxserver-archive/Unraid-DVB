f13c45d7938034860aad87a49ae068b9d66679f7
Neil <chbmb@linuxserver.io>
2017-02-04 15:49:17 +0000

    Update variables.sh

--- files/variables.sh
+++ files/variables.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # set our package list
-slack_package_list=(autoconf automake binutils cpio flex gc gcc gcc-g++ git glibc glibc-solibs guile kernel-headers kernel-modules libcgroup libgudev libmpc libtool libunistring m4 make mercurial mpfr ncurses patch perl pkg-config python sqlite)
+slack_package_current=(autoconf automake binutils cpio flex gc gcc gcc-g++ git glibc glibc-solibs guile kernel-headers kernel-modules libcgroup libgudev libmpc libtool libunistring m4 make mercurial mpfr ncurses patch perl pkg-config python sqlite)
+slack_package_14.2=(bc lftp)
 
 # current TBS Drivers See http://www.tbsiptv.com/downloads?path=3
 TBS="161031"
@@ -9,20 +10,14 @@ TBS="161031"
 # current LibreELEC Release - See https://github.com/LibreELEC/dvb-firmware/releases
 LE="1.2.1"
 
-# current OpenELEC Release - See https://github.com/CvH/dvb-firmware/releases
-OE="1.11"
-
 # current Digital Devices Github release - See https://github.com/DigitalDevices/dddvb/releases
 DD="0.9.28"
 
-# current Date (DDExp Version)
+# current Date (DDExp & TBS OS Version)
 DATE=$(date +'%d%m%y')
 
 # find our working folder
 D="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
-
-#Unraid Kernel Version
-UNRAID-KERNEL="uname -r"
 
 # clean up old files if they exist
 [[ -f "$D"/FILE_LIST ]] && rm "$D"/FILE_LIST
@@ -32,14 +27,23 @@ UNRAID-KERNEL="uname -r"
 VERSION="$(cat /etc/unraid-version | tr "." - | cut -d '"' -f2)"
 
 # get slackware64-current FILE_LIST
-wget -nc http://mirrors.slackware.com/slackware/slackware64-current/slackware64/FILE_LIST
+wget -nc http://mirrors.slackware.com/slackware/slackware64-current/slackware64/FILE_LIST -O $D/FILE_LIST_CURRENT
 
-#python_url="https://github.com/dmacias72/unRAID-plugins/raw/master/packages/6.2/python-2.7.12-x86_64-2.txz"
-slack_package_urlbase="http://mirrors.slackware.com/slackware/slackware64-current/slackware64"
+slack_package_current_urlbase="http://mirrors.slackware.com/slackware/slackware64-current/slackware64"
 
-for i in "${slack_package_list[@]}"
+for i in "${slack_package_current[@]}"
 do
-package_locations=$(grep "\<$i-[[:digit:]].*.txz$" FILE_LIST | cut -b 53-9001)
-echo "$slack_package_urlbase""$package_locations" >> "$D"/URLS
+package_locations=$(grep "\<$i-[[:digit:]].*.txz$" FILE_LIST_CURRENT | cut -b 53-9001)
+echo "$slack_package_current_urlbase""$package_locations" >> "$D"/CURRENTURLS
 done
-echo "$python_url" >> "$D"/URLS
+
+# get slackware64-14.2 FILE_LIST
+wget -nc http://mirrors.slackware.com/slackware/slackware64-14.2/slackware64/FILE_LIST -O $D/FILE_LIST_14.2
+
+slack_package_14.2_urlbase="http://mirrors.slackware.com/slackware/slackware64-14.2/slackware64"
+
+for i in "${slack_package_14.2[@]}"
+do
+package_locations=$(grep "\<$i-[[:digit:]].*.txz$" FILE_LIST_14.2 | cut -b 53-9001)
+echo "$slack_package_14.2_urlbase""$package_locations" >> "$D"/14.2URLS
+done
