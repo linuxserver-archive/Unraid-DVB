66263e19becbf944fc656208f9a75086e9723367
Neil <chbmb@linuxserver.io>
2017-02-04 15:50:13 +0000

    Update variables.sh

--- files/variables.sh
+++ files/variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+#!/bin/bash
+
 # set our package list
 slack_package_current=(autoconf automake binutils cpio flex gc gcc gcc-g++ git glibc glibc-solibs guile kernel-headers kernel-modules libcgroup libgudev libmpc libtool libunistring m4 make mercurial mpfr ncurses patch perl pkg-config python sqlite)
-slack_package_14.2=(bc lftp)
+slack_package_142=(bc lftp)
 
 # current TBS Drivers See http://www.tbsiptv.com/downloads?path=3
 TBS="161031"
@@ -36,14 +38,15 @@ do
 package_locations=$(grep "\<$i-[[:digit:]].*.txz$" FILE_LIST_CURRENT | cut -b 53-9001)
 echo "$slack_package_current_urlbase""$package_locations" >> "$D"/CURRENTURLS
 done
+echo "$python_url" >> "$D"/CURRENTURLS
 
 # get slackware64-14.2 FILE_LIST
-wget -nc http://mirrors.slackware.com/slackware/slackware64-14.2/slackware64/FILE_LIST -O $D/FILE_LIST_14.2
+wget -nc http://mirrors.slackware.com/slackware/slackware64-14.2/slackware64/FILE_LIST -O $D/FILE_LIST_142
 
-slack_package_14.2_urlbase="http://mirrors.slackware.com/slackware/slackware64-14.2/slackware64"
+slack_package_142_urlbase="http://mirrors.slackware.com/slackware/slackware64-14.2/slackware64"
 
-for i in "${slack_package_14.2[@]}"
+for i in "${slack_package_142[@]}"
 do
-package_locations=$(grep "\<$i-[[:digit:]].*.txz$" FILE_LIST_14.2 | cut -b 53-9001)
-echo "$slack_package_14.2_urlbase""$package_locations" >> "$D"/14.2URLS
+package_locations=$(grep "\<$i-[[:digit:]].*.txz$" FILE_LIST_142 | cut -b 53-9001)
+echo "$slack_package_142_urlbase""$package_locations" >> "$D"/142URLS
 done
