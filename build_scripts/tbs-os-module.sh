bc278e2bf1195556d32596d0e928aba99ca8696c
Neil <chbmb@linuxserver.io>
2017-06-03 23:46:00 +0100

    First draft

--- build_scripts/tbs-os-module.sh
+++ build_scripts/tbs-os-module.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-###Run kernel_compile.sh prior to running a module###
+###Run kernel_compile.sh prior to running a module - shouldn't be required after v6.4###
+##Will need to install packages##
 
 ##Pull variables from github
 wget -nc https://raw.githubusercontent.com/CHBMB/Unraid-DVB/master/files/variables.sh
 . "$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"/variables.sh
 
 ##Remove any files remaining in /lib/modules/ & /lib/firmware/
-cd $D
-find /lib/modules/$(uname -r) -type f -exec rm -rf {} \;
-find /lib/firmware -type f -exec rm -rf {} \;
+#cd $D
+#find /lib/modules/$(uname -r) -type f -exec rm -rf {} \;
+#find /lib/firmware -type f -exec rm -rf {} \;
 
 #Restore default /lib/modules/ & /lib/firmware/
-rsync -av $D/lib/modules/$(uname -r)/ /lib/modules/$(uname -r)/
-rsync -av $D/lib/firmware/ /lib/firmware/
+#rsync -av $D/lib/modules/$(uname -r)/ /lib/modules/$(uname -r)/
+#rsync -av $D/lib/firmware/ /lib/firmware/
 
 #Create bzroot-tbs files from master
-rsync -avr $D/bzroot-master-$VERSION/ $D/bzroot-tbs-os
+#rsync -avr $D/bzroot-master-$VERSION/ $D/bzroot-tbs-os
+
+##Unmount bzmodules and make rw
+cp -r /lib/modules /tmp
+umount -l /lib/modules/
+rm -rf  /lib/modules
+mv -f  /tmp/modules /lib
+
+##Unount bzfirmware and make rw
+cp -r /lib/firmware /tmp
+umount -l /lib/firmware/
+rm -rf  /lib/firmware
+mv -f  /tmp/firmware /lib
 
 ##Open Source DVB-ST build
 cd $D
@@ -32,13 +45,14 @@ make install
 mkdir -p $D/tbs-os-firmware/
 cd $D/tbs-os-firmware/
 wget http://www.tbsdtv.com/download/document/linux/tbs-tuner-firmwares_v1.0.tar.bz2
-tar jxvf tbs-tuner-firmwares_v1.0.tar.bz2 -C $D/bzroot-tbs-os/lib/firmware/
+tar jxvf tbs-tuner-firmwares_v1.0.tar.bz2 -C /lib/firmware/
 
 #Copy firmware to bzroot
-find /lib/modules/$(uname -r) -type f -exec cp -r --parents '{}' $D/bzroot-tbs-os/ \;
-find /lib/firmware/ -type f -exec cp -r --parents '{}' $D/bzroot-tbs-os/ \;
+#find /lib/modules/$(uname -r) -type f -exec cp -r --parents '{}' $D/bzroot-tbs-os/ \;
+#find /lib/firmware/ -type f -exec cp -r --parents '{}' $D/bzroot-tbs-os/ \;
 
 #Create /etc/unraid-media to identify type of mediabuild and copy to bzroot
+## NEEDS CHANGING TO A LOCATION IN /lib/firmware or /lib/modules
 echo base=\"TBS \(Open Source\) \& LibreELEC ATSC-C, DVB-C, DVB-S\(2\) \& DVB-T\(2\)\" > $D/bzroot-tbs-os/etc/unraid-media
 echo driver=\"$DATE\" >> $D/bzroot-tbs-os/etc/unraid-media
 
@@ -46,21 +60,25 @@ echo driver=\"$DATE\" >> $D/bzroot-tbs-os/etc/unraid-media
 mkdir -p $D/$VERSION/tbs-os/
 cp $D/bzroot-tbs-os/etc/unraid-media $D/$VERSION/tbs-os/
 
+##Make new bzmodules and bzfirmware
+mkdir -p $D/$VERSION/stock/
+mksquashfs /lib/modules $D/$VERSION/tbs-os/bzmodules -noappend
+mksquashfs /lib/firmware $D/$VERSION/tbs-os/bzfirmware -noappend
+
 #Package Up bzroot
-cd $D/bzroot-tbs-os
-find . | cpio -o -H newc | xz --format=lzma > $D/$VERSION/tbs-os/bzroot
+#cd $D/bzroot-tbs-os
+#find . | cpio -o -H newc | xz --format=lzma > $D/$VERSION/tbs-os/bzroot
 
 #Package Up bzimage
-cp -f $D/kernel/arch/x86/boot/bzImage $D/$VERSION/tbs-os/bzimage
+#cp -f $D/kernel/arch/x86/boot/bzImage $D/$VERSION/tbs-os/bzimage
 
 #Copy default bzroot-gui
-cp -f $D/unraid/bzroot-gui $D/$VERSION/tbs-os/bzroot-gui
+#cp -f $D/unraid/bzroot-gui $D/$VERSION/tbs-os/bzroot-gui
 
 #MD5 calculation of files
 cd $D/$VERSION/tbs-os/
-md5sum bzroot > bzroot.md5
-md5sum bzimage > bzimage.md5
-md5sum bzroot-gui > bzroot-gui.md5
+md5sum bzmodules > bzmodules.md5
+md5sum bzfirmware > bzfirmware.md5
 
 #Return to original directory
 cd $D
