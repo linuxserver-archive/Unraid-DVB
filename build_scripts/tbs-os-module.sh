abf52413267e79741c28fda0ef0ca3a05ce1f463
Neil <chbmb@linuxserver.io>
2017-06-04 00:23:28 +0100

    Update tbs-os-module.sh

--- build_scripts/tbs-os-module.sh
+++ build_scripts/tbs-os-module.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-###Run kernel_compile.sh prior to running a module - shouldn't be required after v6.4###
-##Will need to install packages##
-
 ##Pull variables from github
 wget -nc https://raw.githubusercontent.com/CHBMB/Unraid-DVB/master/files/variables.sh
 . "$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"/variables.sh
 
-##Remove any files remaining in /lib/modules/ & /lib/firmware/
-#cd $D
-#find /lib/modules/$(uname -r) -type f -exec rm -rf {} \;
-#find /lib/firmware -type f -exec rm -rf {} \;
+##Install packages
+[ ! -d "$D/packages" ] && mkdir $D/packages
+  wget -nc -P $D/packages -i $D/URLS_CURRENT
+  installpkg $D/packages/*.*
 
-#Restore default /lib/modules/ & /lib/firmware/
-#rsync -av $D/lib/modules/$(uname -r)/ /lib/modules/$(uname -r)/
-#rsync -av $D/lib/firmware/ /lib/firmware/
+#Download patchutils & Proc-ProcessTable
+mkdir $D/packages
+cd $D/packages
+wget -nc https://github.com/CHBMB/Unraid-DVB/raw/master/files/patchutils-0.3.4-x86_64-2.tgz
+wget -nc https://github.com/CHBMB/Unraid-DVB/raw/master/files/Proc-ProcessTable-0.53-x86_64-1.tgz
 
-#Create bzroot-tbs files from master
-#rsync -avr $D/bzroot-master-$VERSION/ $D/bzroot-tbs-os
+##Download Unraid
+cd $D
+if [ -e $D/unRAIDServer-"$(grep -o '".*"' /etc/unraid-version | sed 's/"//g')"-x86_64.zip]; then
+ unzip unRAIDServer-"$(grep -o '".*"' /etc/unraid-version | sed 's/"//g')"-x86_64.zip -d $D/unraid
+else
+  wget -nc http://dnld.lime-technology.com/next/unRAIDServer-"$(grep -o '".*"' /etc/unraid-version | sed 's/"//g')"-x86_64.zip
+  unzip unRAIDServer-"$(grep -o '".*"' /etc/unraid-version | sed 's/"//g')"-x86_64.zip -d $D/unraid
+fi
 
 ##Unmount bzmodules and make rw
 cp -r /lib/modules /tmp
@@ -47,33 +52,18 @@ cd $D/tbs-os-firmware/
 wget http://www.tbsdtv.com/download/document/linux/tbs-tuner-firmwares_v1.0.tar.bz2
 tar jxvf tbs-tuner-firmwares_v1.0.tar.bz2 -C /lib/firmware/
 
-#Copy firmware to bzroot
-#find /lib/modules/$(uname -r) -type f -exec cp -r --parents '{}' $D/bzroot-tbs-os/ \;
-#find /lib/firmware/ -type f -exec cp -r --parents '{}' $D/bzroot-tbs-os/ \;
-
-#Create /etc/unraid-media to identify type of mediabuild and copy to bzroot
-## NEEDS CHANGING TO A LOCATION IN /lib/firmware or /lib/modules
-echo base=\"TBS \(Open Source\) \& LibreELEC ATSC-C, DVB-C, DVB-S\(2\) \& DVB-T\(2\)\" > $D/bzroot-tbs-os/etc/unraid-media
-echo driver=\"$DATE\" >> $D/bzroot-tbs-os/etc/unraid-media
+#Create /lib/firmware/unraid-media to identify type of mediabuild
+echo base=\"TBS \(Open Source\) \& LibreELEC ATSC-C, DVB-C, DVB-S\(2\) \& DVB-T\(2\)\" > /lib/firmware/unraid-media
+echo driver=\"$DATE\" >> /lib/firmware/unraid-media
 
 #Copy /etc/unraid-media to identify type of mediabuild to destination folder
 mkdir -p $D/$VERSION/tbs-os/
-cp $D/bzroot-tbs-os/etc/unraid-media $D/$VERSION/tbs-os/
+cp /lib/firmware/unraid-media $D/$VERSION/tbs-os/
 
 ##Make new bzmodules and bzfirmware
 mkdir -p $D/$VERSION/stock/
 mksquashfs /lib/modules $D/$VERSION/tbs-os/bzmodules -noappend
 mksquashfs /lib/firmware $D/$VERSION/tbs-os/bzfirmware -noappend
-
-#Package Up bzroot
-#cd $D/bzroot-tbs-os
-#find . | cpio -o -H newc | xz --format=lzma > $D/$VERSION/tbs-os/bzroot
-
-#Package Up bzimage
-#cp -f $D/kernel/arch/x86/boot/bzImage $D/$VERSION/tbs-os/bzimage
-
-#Copy default bzroot-gui
-#cp -f $D/unraid/bzroot-gui $D/$VERSION/tbs-os/bzroot-gui
 
 #MD5 calculation of files
 cd $D/$VERSION/tbs-os/
@@ -83,4 +73,10 @@ md5sum bzfirmware > bzfirmware.md5
 #Return to original directory
 cd $D
 
-##Will need to restore default bzfirmware and bzroot at end.
+##Restore /lib/modules/ & /lib/firmware/
+rm -rf  /lib/modules
+rm -rf  /lib/firmware
+mkdir /lib/modules
+mkdir /lib/firmware
+mount /unraid/bzmodules /lib/firmware -t squashfs -o loop
+mount /unraid/bzfirmware /lib/firmware -t squashfs -o loop
