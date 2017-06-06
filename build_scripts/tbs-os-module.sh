afd2b11b334ec29df4312f49bde0ab86e92a8832
Neil <chbmb@linuxserver.io>
2017-06-06 13:55:48 +0100

    Update tbs-os-module.sh

--- build_scripts/tbs-os-module.sh
+++ build_scripts/tbs-os-module.sh
@@ -4,25 +4,15 @@
 wget -nc https://raw.githubusercontent.com/CHBMB/Unraid-DVB/master/files/variables.sh
 . "$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"/variables.sh
 
-##Install packages
-[ ! -d "$D/packages" ] && mkdir $D/packages
-  wget -nc -P $D/packages -i $D/URLS_CURRENT
-  installpkg $D/packages/*.*
-
-#Download patchutils & Proc-ProcessTable
-mkdir $D/packages
-cd $D/packages
-wget -nc https://github.com/CHBMB/Unraid-DVB/raw/master/files/patchutils-0.3.4-x86_64-2.tgz
-wget -nc https://github.com/CHBMB/Unraid-DVB/raw/master/files/Proc-ProcessTable-0.53-x86_64-1.tgz
-
-##Download Unraid
-cd $D
-if [ -e $D/unRAIDServer-"$(grep -o '".*"' /etc/unraid-version | sed 's/"//g')"-x86_64.zip]; then
- unzip unRAIDServer-"$(grep -o '".*"' /etc/unraid-version | sed 's/"//g')"-x86_64.zip -d $D/unraid
-else
-  wget -nc http://dnld.lime-technology.com/next/unRAIDServer-"$(grep -o '".*"' /etc/unraid-version | sed 's/"//g')"-x86_64.zip
-  unzip unRAIDServer-"$(grep -o '".*"' /etc/unraid-version | sed 's/"//g')"-x86_64.zip -d $D/unraid
-fi
+##Restore /lib/modules/ & /lib/firmware/
+umount -l /lib/modules/
+umount -l /lib/firmware/
+rm -rf  /lib/modules
+rm -rf  /lib/firmware
+mkdir /lib/modules
+mkdir /lib/firmware
+mount /boot/bzmodules /lib/modules -t squashfs -o loop
+mount /boot/bzfirmware /lib/firmware -t squashfs -o loop
 
 ##Unmount bzmodules and make rw
 cp -r /lib/modules /tmp
@@ -56,12 +46,12 @@ tar jxvf tbs-tuner-firmwares_v1.0.tar.bz2 -C /lib/firmware/
 echo base=\"TBS \(Open Source\) \& LibreELEC ATSC-C, DVB-C, DVB-S\(2\) \& DVB-T\(2\)\" > /lib/firmware/unraid-media
 echo driver=\"$DATE\" >> /lib/firmware/unraid-media
 
-#Copy /etc/unraid-media to identify type of mediabuild to destination folder
+#Copy /lib/firmware/unraid-media to identify type of mediabuild to destination folder
 mkdir -p $D/$VERSION/tbs-os/
 cp /lib/firmware/unraid-media $D/$VERSION/tbs-os/
 
 ##Make new bzmodules and bzfirmware
-mkdir -p $D/$VERSION/stock/
+mkdir -p $D/$VERSION/tbs-os/
 mksquashfs /lib/modules $D/$VERSION/tbs-os/bzmodules -noappend
 mksquashfs /lib/firmware $D/$VERSION/tbs-os/bzfirmware -noappend
 
@@ -70,13 +60,9 @@ cd $D/$VERSION/tbs-os/
 md5sum bzmodules > bzmodules.md5
 md5sum bzfirmware > bzfirmware.md5
 
+#Copy necessary stock files
+cp $D/$VERSION/stock/bzimage* $D/$VERSION/tbs-os/
+cp $D/$VERSION/stock/bzroot* $D/$VERSION/tbs-os/
+
 #Return to original directory
 cd $D
-
-##Restore /lib/modules/ & /lib/firmware/
-rm -rf  /lib/modules
-rm -rf  /lib/firmware
-mkdir /lib/modules
-mkdir /lib/firmware
-mount /unraid/bzmodules /lib/firmware -t squashfs -o loop
-mount /unraid/bzfirmware /lib/firmware -t squashfs -o loop
