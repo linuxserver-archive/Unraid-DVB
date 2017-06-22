54dffe930823354bfae407108f575c68e328321e
Neil <chbmb@linuxserver.io>
2017-06-22 18:19:40 +0100

    Update build.sh

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -7,8 +7,6 @@ wget -nc https://raw.githubusercontent.com/CHBMB/Unraid-DVB/master/files/variabl
 $D/kernel-compile-module.sh && \
 $D/libreelec-module.sh && \
 $D/tbs-os-module.sh && \
-$D/tbs-official-dvbst-module.sh && \
-$D/tbs-official-dvbc-module.sh && \
 $D/tbs-crazy-dvbst-module.sh && \
 $D/dd-module.sh && \
 $D/ftp.sh
