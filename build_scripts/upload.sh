60daa8bd1afe5a523e8006706bdaa8593dbccccc
chbmb <chbmb@linuxserver.io>
2018-01-06 15:50:45 +0000

    Update upload.sh

--- build_scripts/upload.sh
+++ build_scripts/upload.sh
@@ -6,5 +6,6 @@ D="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 # current Unraid Version
 VERSION="$(cat /etc/unraid-version | tr "." - | cut -d '"' -f2)"
 
+mkdir -p ~/.ssh/
 ssh-keyscan -H mirror.linuxserver.io >> ~/.ssh/known_hosts
 scp -i /etc/ssh/files.linuxserver.io -r $D/$VERSION/ lsio@mirror.linuxserver.io:/mnt/forum-block/letsencrypt/www/mirror/unraid-dvb-rc/
