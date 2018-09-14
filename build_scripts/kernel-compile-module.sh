100692f61ca5017393f4e873399a6ec71fdacd2a
chbmb <chbmb@linuxserver.io>
2018-09-14 14:37:01 +0100

    Update kernel-compile-module.sh

--- build_scripts/kernel-compile-module.sh
+++ build_scripts/kernel-compile-module.sh
@@ -7,7 +7,7 @@ wget -nc https://raw.githubusercontent.com/CHBMB/Unraid-DVB/nvidia/build_scripts
 ##Install packages
 [ ! -d "$D/packages" ] && mkdir $D/packages
   wget -nc -P $D/packages -i $D/URLS_CURRENT
-  wget -nc -P $D/packages https://github.com/CHBMB/Unraid-DVB/raw/master/files/patchutils-0.3.4-x86_64-2.tgz
+  wget -nc -P $D/packages https://github.com/CHBMB/Unraid-DVB/raw/master/files/patchutils-0.3.4-x86_64-3.tgz
   wget -nc -P $D/packages https://github.com/CHBMB/Unraid-DVB/raw/master/files/Proc-ProcessTable-0.53-x86_64-3.tgz
   installpkg $D/packages/*.*
 
