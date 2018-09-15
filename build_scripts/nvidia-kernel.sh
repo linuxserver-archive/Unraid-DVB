b1a82bbb5c7b815a4fd71b896f5dbe310b484793
chbmb <chbmb@linuxserver.io>
2018-09-15 11:50:21 +0100

    Update nvidia-kernel.sh

--- build_scripts/nvidia-kernel.sh
+++ build_scripts/nvidia-kernel.sh
@@ -2,7 +2,7 @@
 
 ##Pull variables from github
 wget -nc https://raw.githubusercontent.com/CHBMB/Unraid-DVB/master/build_scripts/nvidia-variables.sh
-. "$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"/variables.sh
+. "$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"/nvidia-variables.sh
 
 ##Restore /lib/modules/
 rm -rf  /lib/modules
