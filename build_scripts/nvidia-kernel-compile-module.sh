bc2446edfdd577038fa71332bb3e1d4dcbb4088b
chbmb <chbmb@linuxserver.io>
2018-09-15 11:49:56 +0100

    Update nvidia-kernel-compile-module.sh

--- build_scripts/nvidia-kernel-compile-module.sh
+++ build_scripts/nvidia-kernel-compile-module.sh
@@ -80,5 +80,37 @@ else
   unzip unRAIDServer-"$(grep -o '".*"' /etc/unraid-version | sed 's/"//g')"-x86_64.zip -d $D/unraid
 fi
 
+##Copy default Unraid bz files to folder prior to uploading
+mkdir -p $D/$VERSION/stock/
+cp -f $D/unraid/bzimage $D/$VERSION/stock/
+cp -f $D/unraid/bzroot $D/$VERSION/stock/
+cp -f $D/unraid/bzroot-gui $D/$VERSION/stock/
+cp -f $D/kernel/.config $D/$VERSION/stock/
+
+##Calculate md5 on stock files
+cd $D/$VERSION/stock/
+md5sum bzroot > bzroot.md5
+md5sum bzroot-gui > bzroot-gui.md5
+md5sum .config > .config.md5
+
+##Make new bzmodules and bzfirmware
+mksquashfs /lib/modules/$(uname -r)/ $D/$VERSION/stock/bzmodules -keep-as-directory -noappend
+mksquashfs /lib/firmware $D/$VERSION/stock/bzfirmware -noappend
+
+#Package Up new bzimage
+cp -f $D/kernel/arch/x86/boot/bzImage $D/$VERSION/stock/bzimage
+
+##Make backup of /lib/firmware & /lib/modules
+mkdir -p $D/backup/modules
+cp -r /lib/modules/ $D/backup/
+mkdir -p $D/backup/firmware
+cp -r /lib/firmware/ $D/backup/
+
+##Calculate md5 on new bzimage, bzfirmware & bzmodules
+cd $D/$VERSION/stock/
+md5sum bzimage > bzimage.md5
+md5sum bzmodules > bzmodules.md5
+md5sum bzfirmware > bzfirmware.md5
+
 ##Return to original directory
 cd $D
