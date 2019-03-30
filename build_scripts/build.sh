f0aa0d6687acaf213f94b8e11853e83dff752204
chbmb <chbmb@linuxserver.io>
2019-03-30 13:32:53 +0000

    Update build.sh

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -4,6 +4,18 @@
 wget -nc https://raw.githubusercontent.com/CHBMB/Unraid-DVB/master/build_scripts/variables.sh
 . "$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"/variables.sh
 
+##Pull scripts from Github
+wget https://raw.githubusercontent.com/linuxserver/Unraid-DVB/master/build_scripts/kernel-compile-module.sh
+wget https://raw.githubusercontent.com/linuxserver/Unraid-DVB/master/build_scripts/libreelec-module.sh
+wget https://raw.githubusercontent.com/linuxserver/Unraid-DVB/master/build_scripts/dd-module.sh
+wget https://raw.githubusercontent.com/linuxserver/Unraid-DVB/master/build_scripts/tbs-os-module.sh
+wget https://raw.githubusercontent.com/linuxserver/Unraid-DVB/master/build_scripts/tbs-crazy-dvbst-module.sh
+wget https://raw.githubusercontent.com/linuxserver/Unraid-DVB/master/build_scripts/upload.sh
+
+##Make executable
+chmod +x *.sh
+
+##Run builds
 $D/kernel-compile-module.sh && \
 $D/libreelec-module.sh && \
 $D/tbs-os-module.sh && \
