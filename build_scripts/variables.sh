5f501bb195e5c83cc2b341f1dd56a6984f619429
chbmb <chbmb@linuxserver.io>
2019-03-30 13:29:04 +0000

    Update variables.sh

--- build_scripts/variables.sh
+++ build_scripts/variables.sh
@@ -39,15 +39,37 @@ squashfs-tools \
 zstd \
 )
 
+##Set colours
+export NC='\e[39m' # No Color
+export RED='\e[31m'
+export BLUE='\e[96m'
+export GREEN='\e[92m'
+
+##current RocketRaid R750 Release - See http://www.highpoint-tech.com/BIOS_Driver/R750/Linux/
+echo -e "${BLUE}Variables.sh${NC}    -----    current RocketRaid R750 Release - See http://www.highpoint-tech.com/BIOS_Driver/R750/Linux/"
+export ROCKET="1.2.11-18_06_26"
+export ROCKETSHORT=$(echo $ROCKET | cut -d"-" -f1)
+
+##current RocketRaid RR3740A Release
+echo -e "${BLUE}Variables.sh${NC}    -----    current RocketRaid RR3740A Release - "
+export RR="1.17.0_18_06_15"
+export RRSHORT=$(echo $RR | cut -d"-" -f1)
+
+##current Intel 10GB IXGBE - See https://downloadcenter.intel.com/download/14687/Intel-Network-Adapter-Driver-for-PCIe-Intel-10-Gigabit-Ethernet-Network-Connections-Under-Linux-?product=36773
+echo -e "${BLUE}Variables.sh${NC}    -----    current Intel 10GB IXGBE driver"
+export IXGBE="5.5.3"
+
+##current Intel 10GB IXGBEVF - See https://downloadcenter.intel.com/download/18700/Intel-Network-Adapter-Virtual-Function-Driver-for-Intel-10-Gigabit-Ethernet-Network-Connections?product=36773
+echo -e "${BLUE}Variables.sh${NC}    -----    current Intel 10GB IXGBEVF driver"
+export IXGBEVF="4.5.1"
+
+##tehuti Driver
+echo -e "${BLUE}Variables.sh${NC}    -----    current Tehuti 10GB Driver"
+export TEHUTI="0.3.6.17"
 # current RocketRaid R750 Release - See http://www.highpoint-tech.com/BIOS_Driver/R750/Linux/
 ROCKET="1.2.11-18_06_26"
 
 ROCKETSHORT=$(echo $ROCKET | cut -d"-" -f1)
-
-# current RocketRaid RR3740A Release - 
-RR="1.17.0_18_06_15"
-
-RRSHORT=$(echo $RR | cut -d"-" -f1)
 
 # current LibreELEC Release - See https://github.com/LibreELEC/dvb-firmware/releases
 LE="1.3.1"
@@ -78,3 +100,14 @@ do
 package_locations_current=$(grep "/$i-[[:digit:]].*.txz$" FILE_LIST_CURRENT | cut -b 53-9001)
 echo "$slack_package_current_urlbase""$package_locations_current" >> "$D"/URLS_CURRENT
 done
+
+declare -A oot_driver_map=(
+    	["6.6.6"]="ixgbe"
+      ["6.6.7"]="ixgbe"
+    	["6.7.0"]="ixgbe,tehuti"
+)
+
+export OOT_DRIVERS="${oot_driver_map[$UNRAID_DOWNLOAD_VERSION]}"
+
+##Change to current directory
+echo -e "${BLUE}Variables.sh${NC}    -----    Change to current directory"
